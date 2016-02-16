4ab40e5fe4f93f5baffa756341a83c4751e40a55
Robert Ferentz <RobiFerentz@gmail.com>
2016-02-16 14:10:44 +0200

    more search result parsing

--- routes/command.js
+++ routes/command.js
@@ -5,6 +5,7 @@ var api = require('../api/commandAPI')
 /* GET home page. */
 router.post('/', function(req, res, next) {
   //res.send(req.body.text);
+  res.setHeader('content-type', 'application/json')
   api.handleCommand(req.body.text).then( result => {
 
       res.send(result);
