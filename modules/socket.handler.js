cce13f160e3bd849a4c865f6591eac1fa8315ae8
Gal Noy <gal@tikalk.com>
2016-02-16 13:06:40 +0200

    fixed sockets

--- modules/socket.handler.js
+++ modules/socket.handler.js
@@ -1,14 +1,14 @@
 'use strict'
 
-/**
-* Socket.IO listener
-*/
-var server = http.createServer(app);
-var io = require('socket.io')(app);
+function SocketIO(app) {
+	let server = require('http').Server(app);
+	let io = require('socket.io')(server)
+	io.on('connection', function (socket) {
+		socket.emit('news', { hello: 'world' });
+		socket.on('my other event', function (data) {
+			console.log(data);
+		});
+	})
+}
 
-io.on('connection', function (socket) {
-	socket.emit('news', { hello: 'world' });
-	socket.on('my other event', function (data) {
-		console.log(data);
-	});
-});
+module.exports = SocketIO
