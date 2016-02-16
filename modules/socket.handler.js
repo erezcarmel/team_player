8a05709cc97063183648537ff5122903eb2d335b
Gal Noy <gal@tikalk.com>
2016-02-16 13:54:45 +0200

    removed bug

--- modules/socket.handler.js
+++ modules/socket.handler.js
@@ -21,6 +21,6 @@ app.listen(3001);
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
-    console.log(data);
+    // console.log(data);
   });
 });
