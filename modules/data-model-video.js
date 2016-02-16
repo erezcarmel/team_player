6ead239cb8a4b85ae67a0ca1fc33973e36db0dcb
Gal Noy <gal@tikalk.com>
2016-02-16 15:03:26 +0200

    video list dm

---
+++ modules/data-model-video.js
@@ -0,0 +1,33 @@
+'use strict'
+
+let Datastore = require('nedb')
+let db = new Datastore({ filename: 'temp/VideoList.db', autoload: true })
+
+class VideoList {
+	getAll() {
+		db.find({}, (err, docs) => {
+			if (err) throw Error(err)
+			return docs
+		})
+	}
+	get(id) {
+		db.find({_id: id}, {}, (err, docs) => {
+			if (err) throw Error(err)
+			return docs;
+		});
+	}
+	add(video) {
+		db.insert(video, (err, doc) => {
+			if (err) throw Error(err)
+			return doc
+		})
+	}
+	remove(id) {
+		db.remove({_id: id}, {}, (err, doc) => {
+			if (err) throw Error(err)
+			remove doc
+		});
+	}
+}
+
+module.exports = new VideoList()
