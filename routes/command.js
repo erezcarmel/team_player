c612be8b85ba0e222bb96da85013e1da56cc143c
Gal Noy <gal@tikalk.com>
2016-02-16 10:55:40 +0200

    mistype

--- routes/command.js
+++ routes/command.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  res.send(body.text);
+  res.send(req.body.text);
 });
 
 module.exports = router;
